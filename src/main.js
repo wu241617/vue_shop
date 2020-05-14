4185a66bd9767a6eb6d33c41a45d1cf1fe34af5a
wu241617 <1272689028@qq.com>
2020-05-14 22:46:12 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ import '@/assets/css/global.css'
 import axios from 'axios'
 //配置请求的根路径
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+//通过axios请求拦截器添加 token,保证拥有获取数据的权限
+//axios请求拦截
+axios.interceptors.request.use(config => {
+	//为请求头对象，添加Token验证的Authorization字段
+	config.headers.Authorization = window.sessionStorage.getItem('token');
+	return config;
+})
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
