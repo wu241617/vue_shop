890fa157c6b617ee02f9e417de7b773c65936a6c
wu241617 <1272689028@qq.com>
2020-05-14 23:56:43 +0800

    完善用户列表

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import './plugins/element.js'
 import '@/assets/fonts/iconfont.css'
 //导入全局样式表
 import '@/assets/css/global.css'
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
 //通过axios请求拦截器添加 token,保证拥有获取数据的权限
 //axios请求拦截
 axios.interceptors.request.use(config => {
@@ -14,7 +17,6 @@ axios.interceptors.request.use(config => {
 	config.headers.Authorization = window.sessionStorage.getItem('token');
 	return config;
 })
-
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
